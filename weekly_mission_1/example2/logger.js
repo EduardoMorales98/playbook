25b3cf8bb2b1b7920d5c65ff2cae02a81f1fc649
EduardoMorales <edalmoes98@gmail.com>
2022-04-14 14:25:52 -0500

    Corrección de errores ejemplo 2

--- weekly_mission_1/example2/logger.js
+++ weekly_mission_1/example2/logger.js
@@ -1,38 +1,17 @@
-<<<<<<< HEAD
 // logger.js
 
 // Esta es una función que se guardara en este módulo como info
 exports.info = (message) => {
-    console.log(`info: ${message}`)
-  }
-  
-  // Esta es una función que se guardara en este módulo como verbose
-  exports.verbose = (message) => {
-    console.log(`verbose: ${message}`)
-  }
-  
-  /*
-    const logger = require('./logger')
-    logger.info('This is an informational message')
-    logger.verbose('This is a verbose message')
-   * */
-=======
-// logger.js
+  console.log(`info: ${message}`)
+}
 
-// Esta es una función que se guardara en este módulo como info
-exports.info = (message) => {
-    console.log(`info: ${message}`)
-  }
-  
-  // Esta es una función que se guardara en este módulo como verbose
-  exports.verbose = (message) => {
-    console.log(`verbose: ${message}`)
-  }
-  
-  /*
-    const logger = require('./logger')
-    logger.info('This is an informational message')
-    logger.verbose('This is a verbose message')
-   * */
->>>>>>> ec3083d1dec836b4a2ca529a1ddd8f8dc7b9c576
-  
+// Esta es una función que se guardara en este módulo como verbose
+exports.verbose = (message) => {
+  console.log(`verbose: ${message}`)
+}
+
+/*
+  const logger = require('./logger')
+  logger.info('This is an informational message')
+  logger.verbose('This is a verbose message')
+ * */
--- weekly_mission_1/example2/main.js
+++ weekly_mission_1/example2/main.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 // node main.js
 
 const logger = require('./logger')
 
 logger.info('This is an informational message')
 logger.verbose('This is a verbose message')
-=======
-// node main.js
-
-const logger = require('./logger')
-
-logger.info('This is an informational message')
-logger.verbose('This is a verbose message')
->>>>>>> ec3083d1dec836b4a2ca529a1ddd8f8dc7b9c576
