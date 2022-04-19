24d8a070bf7849afe612fcf01ac34263dc8ac37d
EduardoMorales <edalmoes98@gmail.com>
2022-04-19 12:28:32 -0500

    Agregando clasesy modularización

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const lalo=new MissionCommander("Lalo")
+console.log(lalo.name)
+
+const vale=new MissionCommander("Vale")
+console.log(vale.name)
