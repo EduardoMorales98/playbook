7f8b07779b42f510624d4a82f178a029519e84ee
EduardoMorales <edalmoes98@gmail.com>
2022-04-19 19:34:37 -0500

    Agregando TDD requerimiento 2

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -2,14 +2,14 @@ const User = require('./../../app/models/User');
 describe("Unit Test for User class", () => {
     test('Create an User object', () => {
         //Aqui invocas el código que vas a usar en tu app
-        const user=new User(1,"eduardomorales","Eduardo","Bio","dateCreated","lastUpdated");
+        const user=new User(1,"eduardomorales","Eduardo","Bio");
         //Aquí validas los resultados de ese código
         //Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha(valor esperado)
         expect(user.id).toBe(1)
         expect(user.userName).toBe("eduardomorales")
         expect(user.name).toBe("Eduardo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()//verifica que el valor no sea undefine
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
