7705d025b4c36d1788d2f54eaa56dd592ca74251
EduardoMorales <edalmoes98@gmail.com>
2022-04-22 23:03:24 -0500

    Agregando endpoint DELETE /v1/explorers/:id

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -7,7 +7,7 @@ app.use(express.json()) // Indicamos que usaremos JSON
 const port = 3000
 
 //HTTP METHODS
-app.get('/v1/explorers',(req,res)=>{
+app.get('/v1/explorers',(req,res)=>{//para obtener todos los explorers
     console.log(`Api Explorers GET ALL request ${new Date()}`)
     const explorer1={id:1,name:"Eduardo1"}
     const explorer2={id:2,name:"Eduardo2"}
@@ -18,7 +18,7 @@ app.get('/v1/explorers',(req,res)=>{
     res.status(200).json(explorers)
 })
 
-app.get('/v1/explorers/:id',(req,res)=>{
+app.get('/v1/explorers/:id',(req,res)=>{//para obtener uno mediante id
     console.log(`Api Explorers GET request ${new Date()}`)
     console.log(`Getting explorers with id: ${req.params.id}`)
     const explorer={id:1,name:"Eduardo"}
@@ -26,14 +26,14 @@ app.get('/v1/explorers/:id',(req,res)=>{
     res.status(200).json(explorer)
 })
 
-app.post('/v1/explorers',(req,res)=>{
+app.post('/v1/explorers',(req,res)=>{//es para crear uno nuevo
     console.log(`Api Explorers POST request ${new Date()}`)
     const requesBody=req.body//Parámetros de un cliente
     console.log(requesBody)//Imprime lo que se recibió del cliente
     res.status(201).json({message: "Created"})
 })
 
-app.put('/v1/explorers/:id',(req,res)=>{
+app.put('/v1/explorers/:id',(req,res)=>{//es para actualizar uno
     console.log(`Api Explorers PUT request ${new Date()}`)
     const requesBody=req.body//Parametros para actualizar
     console.log(`Parámetros para actualizar ${requesBody}`)//
@@ -42,7 +42,15 @@ app.put('/v1/explorers/:id',(req,res)=>{
     res.status(200).json({message:"Updated!"})
 })
 
-// Con esto inicializamos esta app
+app.delete('/v1/explorers/:id',(req,res)=>{
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const  requesBody=req.body//Parametro de un cliente
+    res.status(200).json({message: "Deleted"})
+})
+
+// Con esto inicializamos esta app, para correrla es "node app.js"
+//para parla es con Ctrl+c
 app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
