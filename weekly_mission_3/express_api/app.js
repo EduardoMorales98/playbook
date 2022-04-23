8be968ef5193bb8c9644828e298c039be9b511e1
EduardoMorales <edalmoes98@gmail.com>
2022-04-22 22:26:05 -0500

    Agregando endpoint POST /v1/explorers

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -26,6 +26,13 @@ app.get('/v1/explorers/:id',(req,res)=>{
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers',(req,res)=>{
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requesBody=req.body//Parámetros de un cliente
+    console.log(requesBody)//Imprime lo que se recibió del cliente
+    res.status(201).json({message: "Created"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
